0a42665ebdd3010daa9f012641a3df12e3dc6999
RickyLennon <156732664+RickyLennon@users.noreply.github.com>
2024-07-19 22:27:56 -0600

    Update PIA.R

--- PIA.R
+++ PIA.R
@@ -1,12 +1,25 @@
+setwd()
+datos <- read.csv("DatosMEM.csv", sep = ",")[c(1:37),-c(6:10)]
+
 install.packages("MVN")
-library("MVN")
+library(MVN)
 
+mvn(datos, mvnTest = "mardia")
 
-setwd("/Users/astro/Desktop/MCD/Tetra 2/MEM")
-datos <- read.table("Datos MEM.csv", sep = ",", header = T)
-mvn(datos, mvnTest = "hz")
+install.packages("MASS")
+install.packages("dplyr")
+library(MASS)
+library(dplyr)
 
+mean_vector <- colMeans(datos)
+cov_matrix <- cov(datos)
+n_new_samples <- 25
+set.seed(123)
+new_data <- mvrnorm(n = n_new_samples, mu = mean_vector, Sigma = cov_matrix)
 
-help("mvn")
-"Si usamos la prube Henze-Zirkler, hay que buscar cuál es su estadístico de prueba y su
-región de rechazo"
+new_data_df <- as.data.frame(new_data)
+colnames(new_data_df) <- colnames(datos)
+
+expanded_data <- bind_rows(datos, new_data_df)
+mvn(expanded_data, mvnTest = "mardia")
+
